1ed1d2a356421eaf7aea9816e7f990ee7e6869d5
fefefe12 <cookiesxd419@gmail.com>
2025-04-05 19:10:57 +0530

    Update userbot.py

--- SONALI_MUSIC/core/userbot.py
+++ SONALI_MUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.one.join_chat("ZOYU_SUPPORT")
+                await self.one.join_chat("THE_INCRICIBLE")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.two.join_chat("ZOYU_SUPPORT")
+                await self.one.join_chat("THE_INCRICIBLE")
             except:
                 pass
             assistants.append(2)
